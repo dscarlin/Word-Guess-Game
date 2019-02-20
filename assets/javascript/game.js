928165dbd5c906b316684f67747db284817a411c
David Carlin <dave.carlin86@gmail.com>
2019-02-20 17:02:03 -0500

    fixed event list syntax on body touchstart dinoReturn

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -156,7 +156,7 @@ dinopic.ondblclick = roar;
 
 //Auxillary UI experience -- mobile
 dinopad.addEventListener('touchstart', dinoAction);
-document.body.ontouchstart = function(){ dinoRetun};
+document.body.addEventListener('touchstart', dinoRetun);
 //dinopad.addEventListener('touchend', dinoReturn );
 dinopad.addEventListener('touchstart', roar);
 
